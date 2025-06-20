55a0f576930e2ad4857e14b659576d16544b75f9
desp0o <tornike.despotashvili@gmail.com>
2025-06-21 00:22:50 +0400

    update chat with llm

--- swift-FoundationModels/ContentView.swift
+++ swift-FoundationModels/ContentView.swift
@@ -13,6 +13,19 @@ struct ContentView: View {
   @State private var question: String = ""
   var body: some View {
     VStack {
+      ScrollView {
+        VStack(alignment: .leading) {
+          ForEach(Array(vm.chat).reversed(), id: \.key) { question, answer in
+            VStack(alignment: .leading, spacing: 6) {
+              Text("Q: \(question)").bold()
+              Text("A: \(answer)")
+            }
+            .padding(.top, 8)
+          }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+      }
+      
       TextField("Ask Question", text: $question)
         .frame(height: 40)
         .padding(.horizontal, 20)
@@ -27,8 +40,19 @@ struct ContentView: View {
         Text("ASK")
       }
       .buttonStyle(.glass)
-      
-      Text(vm.answer)
+    }
+    .animation(.default, value: vm.chat)
+    .overlay {
+      if vm.isLoading {
+        VStack {
+          ProgressView()
+            .scaleEffect(2)
+            .tint(.blue)
+            .padding(20)
+            .glassEffect(in: RoundedRectangle(cornerRadius: 10))
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+      }
     }
     .padding()
   }
@@ -43,18 +67,25 @@ import Observation
 @MainActor
 @Observable
 final class ContentViewModel {
-  var answer: String = ""
+  var chat: [String: String] = [:]
+  var isLoading: Bool = false
   
   func askQuestion(question: String) {
     let session = LanguageModelSession()
+    isLoading = true
     
     Task {
+      defer {
+        isLoading = false
+      }
+      
       do {
         let response = try await session.respond(to: question)
-        answer = response.content
+        chat[question] = response.content
       } catch {
         print(error)
       }
     }
   }
 }
+
